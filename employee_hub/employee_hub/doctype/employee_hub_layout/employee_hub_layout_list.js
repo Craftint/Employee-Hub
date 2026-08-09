daf9f2afce882bbf79ff970434146a9478ef9015
Sebin <sebin.freelance@gmail.com>
2026-08-09 19:33:44 +0400

    Feat: Remove Add button from Employee Hub Layout list view

---
+++ employee_hub/employee_hub/doctype/employee_hub_layout/employee_hub_layout_list.js
@@ -0,0 +1,7 @@
+// Employee Hub — developed by Sebin P Sabu (sebin.freelance@gmail.com)
+
+frappe.listview_settings["Employee Hub Layout"] = {
+    onload(listview) {
+        listview.page.clear_primary_action();
+    },
+};
